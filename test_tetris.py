0f2551118f19be652df89287ae2a8070c85b62b3
Jenny Yang <michel3153@gmail.com>
2025-05-11 16:30:32 +0100

    refactor level 1 logics

--- test_tetris.py
+++ test_tetris.py
@@ -1,5 +1,5 @@
 import pytest
-from tetris import drop_block, Grid
+from tetris import drop_block, Grid, Block, print_grid
 from random import randint
 
 blocks = [
@@ -18,42 +18,42 @@ def validate_grid(grid, expected_block, x, y):
                 assert grid[y + i][x + j] == 1
 
 @pytest.mark.parametrize("block, x", [
-    (blocks[0], 0),  # Line block at x=0
-    (blocks[1], 4),  # Square block at x=4
-    (blocks[2], 4), 
-    (blocks[3], 2), 
-    (blocks[4], 6) 
+    (Block.I.value, 0),  # Line block at x=0
+    (Block.O.value, 4),  # Square block at x=4
+    (Block.T.value, 4), 
+    (Block.L.value, 2), 
+    (Block.Z.value, 6) 
 ])
 def test_empty_blocks(block, x):
     grid_object = Grid()
     grid = grid_object.grid
-    expected_y = len(grid) - len(block)
+    expected_y = grid_object.width - len(block)
     drop_block(grid_object, block, x)
-    print(grid)
     validate_grid(grid, block, x, expected_y)
 
 class UsedGrid(Grid):
     def __init__(self, highest_land):
         super().__init__()
-        self.highest_land = len(self.grid) - 1 - highest_land
+        self.highest_land = highest_land
         self.generate_random()
 
     def generate_random(self):
-        for r in range(self.highest_land, len(self.grid)):
-            for c in range(len(self.grid[0])):
+        grid_height = self.height-1
+        for r in range(grid_height, grid_height - self.highest_land, -1):
+            for c in range(self.width):
                 value = randint(0, 1)
                 self.grid[r][c] = value
                 if value == 1:
-                    self.drop_point[c] = min(self.drop_point[c], r)
+                    self.drop_point[c] = r-1
 
 @pytest.mark.parametrize("block, x", [
-    (blocks[0], 1)
+    (Block.I.value, 1)
 ])
 def test_non_empty_block(block, x):
     offset = 2
     grid_object = UsedGrid(offset)
     grid = grid_object.grid
-    expected_y = len(grid) - offset - len(block)
+    expected_y = grid_object.drop_point[x] - len(block) + 1
     drop_block(grid_object, block, x)
     validate_grid(grid, block, x, expected_y)
 
@@ -61,15 +61,16 @@ def test_clear():
     grid_obj = Grid()
     grid = grid_obj.grid
     drop_sequence = [
-        (blocks[2], 5),
-        (blocks[4], 3),
-        (blocks[1], 8),
-        (blocks[2], 0)
+        (Block.T.value, 5),
+        (Block.Z.value, 3),
+        (Block.O.value, 8),
+        (Block.L.value, 0)
     ]
     for block, x in drop_sequence:
         drop_block(grid_obj, block, x)
 
-    expected_row = [0, 1, 0, 0, 1, 1, 1, 0, 1, 1]
-    assert grid[19] == expected_row
-    assert grid[18] == [0]*len(grid[0])    
+    expected_rows = [[1, 0, 0, 0, 0, 0, 0, 0, 0, 0],[1, 0, 0, 0, 0, 0, 0, 0, 0, 0],[1, 0, 0, 0, 1, 1, 1, 0, 1, 1]]
+    for i, row in enumerate(expected_rows):
+        assert grid[17+i] == row
+   
     
--- tetris.py
+++ tetris.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 class Grid:
     def __init__(self, width=10, height=20):
         self.width = width
@@ -5,6 +7,21 @@ class Grid:
         self.grid = [[0 for _ in range(width)] for _ in range(height)]
         self.drop_point = [len(self.grid)-1 for _ in range(len(self.grid[0]))] 
         
+class Block(Enum):
+  I = [[1], [1], [1], [1]]
+  O = [[1, 1], [1, 1]]
+  T = [[0, 1, 0], [1, 1, 1]]
+  L = [[1, 0], [1, 0], [1, 1]]
+  Z = [[0, 1, 1], [1, 1, 0]]       
+
+def mark_cells(grid_object, changing_cells):
+    grid = grid_object.grid
+    drop_point = grid_object.drop_point
+
+    for r, c in changing_cells:
+        grid[r][c] = 1
+        drop_point[c] = min(drop_point[c], r-1)
+
 
 def get_droppable(grid_object, block, x):
     grid = grid_object.grid    
@@ -18,20 +35,38 @@ def get_droppable(grid_object, block, x):
 
     height = len(block)
     for y in range(y_max, y_min-1, -1):
+        changing_cells = []
+        valid = True
+
         for i, row in enumerate(block):
             for j, cell in enumerate(row):
                 r = y-height+1+i
                 c = x+j
+                
+                if r < 0:
+                    valid = False
+                    changing_cells.clear()
+                    break
                 if cell == 1 and grid[r][c] == 0:
-                    grid[r][c] = 1 
-                    drop_point[c] = min(drop_point[c], r-1)
-                else: 
+                    changing_cells.append((r, c))     
+                elif cell == 1 and grid[r][c] == 1:
+                    valid = False
+                    changing_cells.clear()
                     break  
-            return y        
+            if not valid:
+                break
+        if valid:
+            mark_cells(grid_object, changing_cells)
+            return y    
+    raise ValueError("No valid drop position found.")    
 
-def clear_full_row(grid):
+
+
+def clear_full_row(grid_object):
+    grid = grid_object.grid
     drop_point = grid_object.drop_point
     highest_row = min(drop_point)
+
     for i in range(len(grid)-1, highest_row-1, -1):
         clear = False
         for j in range(len(grid[0])):
@@ -41,18 +76,24 @@ def clear_full_row(grid):
                 clear = False
                 break
         if clear:
-            for j in range(len(grid[0])):
-                grid[i][j] = 0
-            
-            for r in range(i-1, 0, -1):
-                grid[r], grid[r+1] = grid[r+1], grid[r]
+            for r in range(i-1, -1, -1):
+                grid[r+1] = grid[r][:] 
+            grid[0] = [0] * len(grid[0])    
 
+def recalculate_drop_points(grid_object):
+    grid = grid_object.grid
 
+    for c in range(grid_object.width):
+        for r in range(grid_object.height):
+            if grid[r][c] == 1:
+                grid_object.drop_point[c] = r - 1
+                break
+        grid_object.drop_point[c] = grid_object.height-1
 
 def drop_block(grid_object, block, x):
     grid = grid_object.grid
 
-    if 0 > x or x + len(block[0]) >= len(grid)-1:
+    if 0 > x or x + len(block[0]) > grid_object.width:
         raise ValueError("x value is not valid.")
     
     drop_point = grid_object.drop_point
@@ -67,7 +108,8 @@ def drop_block(grid_object, block, x):
                 grid[r][c] = 1 
                 drop_point[c] = min(drop_point[c], r-1)
 
-    clear_full_row(grid)            
+    clear_full_row(grid_object) 
+    recalculate_drop_points(grid_object)           
 
 def print_grid(grid):
     print("***********grid************")
@@ -77,20 +119,20 @@ def print_grid(grid):
             print_line += f"{cell} "
         print(print_line)
     print("***************************")    
-grid_object = Grid()
+# grid_object = Grid()
 
-blocks = [
-    [[1], [1], [1], [1]], 
-    [[1, 1], [1, 1]], 
-    [[0, 1, 0], [1, 1, 1]], 
-    [[1, 0], [1, 0], [1, 1]], 
-    [[0, 1, 1], [1, 1, 0]]
-]
-drop_block(grid_object, [[0, 1, 0], [1, 1, 1]], 5)     
-print_grid(grid_object.grid)  
-drop_block(grid_object, [[0, 1, 1], [1, 1, 0]], 3) 
-print_grid(grid_object.grid)
-drop_block(grid_object, [[1, 1], [1, 1]], 8)
-print_grid(grid_object.grid)
-drop_block(grid_object, [[0, 1, 0], [1, 1, 1]], 0)
-print_grid(grid_object.grid)
+# blocks = [
+#     [[1], [1], [1], [1]], 
+#     [[1, 1], [1, 1]], 
+#     [[0, 1, 0], [1, 1, 1]], 
+#     [[1, 0], [1, 0], [1, 1]], 
+#     [[0, 1, 1], [1, 1, 0]]
+# ]
+# drop_block(grid_object, [[0, 1, 0], [1, 1, 1]], 5)     
+# print_grid(grid_object.grid)  
+# drop_block(grid_object, [[0, 1, 1], [1, 1, 0]], 3) 
+# print_grid(grid_object.grid)
+# drop_block(grid_object, [[1, 1], [1, 1]], 8)
+# print_grid(grid_object.grid)
+# drop_block(grid_object, [[0, 1, 0], [1, 1, 1]], 0)
+# print_grid(grid_object.grid)
